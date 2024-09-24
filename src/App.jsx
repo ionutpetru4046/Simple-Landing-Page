e3f8436448ea7e88472e002e2d8df13f61fd86aa
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-24 22:48:07 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar"
+
+const App = () => {
+  return (
+    <main className="overflow-x-hidden antialiased text-neutral-800">
+      <Navbar />
+    </main>
+  )
+}
+
+export default App
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import logo from "../assets/logo.webp"
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+    }
+  return (
+    <nav className="border-b-2">
+        <div className="max-w-7xl mx-auto flex justify-between items-center py-8">
+            <div className="pl-2">
+                <a href="">
+                    <img src={logo} width={150} height={15} alt="Vastuspaze" />
+                </a>
+            </div>
+
+            <div className="md:hidden">
+                <button onClick={toggleMenu} className="text-2xl pr-2"></button>
+            </div>
+
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
