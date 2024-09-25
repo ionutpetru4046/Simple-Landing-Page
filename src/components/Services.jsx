58662aadf83601b8a78840015dd319520e81d7e4
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-25 09:26:33 +0100

    added to service section h3 and paragraph

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -13,7 +13,16 @@ const Services = () => {
                         <img src={service.image} alt={service.title} 
                         className="w-full h-auto object-cover rounded-lg" />
                     </div>
-                    <div className=""></div>
+                    <div className={`lg:w-1/2 flex flex-col ${
+                        index % 2 === 0 ? "lg:pl-12" : "lg:pr-12"
+                    }`}>
+                        <h3 className="text-xl lg:text-2xl font-medium mb-2">
+                        {service.title}
+                        </h3>
+                        <p className="mb-4 lg:tracking-wide text-lg lg:text-xl lg:leading-9">
+                            {service.description}
+                        </p>
+                    </div>
                 </div>
             ))}
         </div>
