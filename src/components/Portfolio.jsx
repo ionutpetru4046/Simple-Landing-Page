671bdef9a65f10ee00f55e555107bc719b809af0
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-25 10:45:15 +0100

    h2 title added to portfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,11 @@
 const Portfolio = () => {
   return (
-    <section className="">
-        
+    <section className="max-w-7xl mx-auto border-b-2" id="portfolio">
+        <div className="mb-20">
+            <h2 className="text-xl lg:text-3xl tracking-tight text-center uppercase 
+            mt-20 mb-12">Portfolio</h2>
+            <div className=""></div>
+        </div>
     </section>
   )
 }
