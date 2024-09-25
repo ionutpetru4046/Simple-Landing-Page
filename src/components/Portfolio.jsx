f9acf677f7a24f42972abaa5cdf4abfab056d5a2
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-25 10:52:34 +0100

    images added to portfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,10 +1,19 @@
+import { PORTFOLIO_PROJECTS } from "../constants"
+
 const Portfolio = () => {
   return (
     <section className="max-w-7xl mx-auto border-b-2" id="portfolio">
         <div className="mb-20">
             <h2 className="text-xl lg:text-3xl tracking-tight text-center uppercase 
             mt-20 mb-12">Portfolio</h2>
-            <div className=""></div>
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+                {PORTFOLIO_PROJECTS.map((project) => (
+                    <div key={project.id} className="group relative overflow-hidden rounded-3xl 
+                    mx-4">
+                        <img src={project.image} />
+                    </div>
+                ))}
+            </div>
         </div>
     </section>
   )
