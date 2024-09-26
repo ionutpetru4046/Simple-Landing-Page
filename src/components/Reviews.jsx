68e41cdaa942bbd47d7da116b27e14d086484442
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-26 17:33:00 +0100

    added h3 to the section

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -3,6 +3,7 @@ const Reviews = () => {
     <section className="max-w-7xl mx-auto border-b-2" id="reviews">
         <div className="my-20">
           <h2 className="text-xl lg:text-3xl tracking-tight text-center uppercase mb-12">Reviews</h2>
+          <h3 className=""> What our customers says</h3>
         </div>
     </section>
   )
