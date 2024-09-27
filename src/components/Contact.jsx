7689b4658f25f4c188dbae86292b5a147a2f3119
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-27 15:54:29 +0100

    h2 added to the section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,12 @@
 const Contact = () => {
   return (
-    <section className="">
-        
+    <section className="max-w-7xl mx-auto" id="contact">
+        <div className="my-20">
+            <h2 className="text-xl lg:text-3xl tracking-tight text-center 
+            uppercase mb-12">
+                Contact
+            </h2>
+        </div>
     </section>
   )
 }
