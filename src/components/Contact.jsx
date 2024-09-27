d04cc09e17e5edc041336622c1dba8f4a29bd6a6
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-27 16:00:29 +0100

    remixicon icon added to contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,6 @@
+import { RiPhoneLine } from "@remixicon/react"
+import { CONTACT_INFO } from "../constants"
+
 const Contact = () => {
   return (
     <section className="max-w-7xl mx-auto" id="contact">
@@ -6,6 +9,14 @@ const Contact = () => {
             uppercase mb-12">
                 Contact
             </h2>
+            <p className="max-w-2xl text-lg mb-12 text-center mx-auto">
+                {CONTACT_INFO.text}
+            </p>
+            <div className="flex flex-col lg:flex-row justify-around mx-4">
+                <div className="flex mb-8 lg:mb-0">
+                    <RiPhoneLine className="text-3xl mr-4" />
+                </div>
+            </div>
         </div>
     </section>
   )
