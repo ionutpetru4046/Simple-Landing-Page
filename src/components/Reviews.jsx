16d0fdf9083fb72d2b6b8aa4ab64cd3527b0c60b
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-27 15:29:37 +0100

    some changes added to the reviews section

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,9 +1,21 @@
+import { REVIEWS } from "../constants"
+
 const Reviews = () => {
   return (
     <section className="max-w-7xl mx-auto border-b-2" id="reviews">
         <div className="my-20">
           <h2 className="text-xl lg:text-3xl tracking-tight text-center uppercase mb-12">Reviews</h2>
-          <h3 className=""> What our customers says</h3>
+          <p className="max-w-2xl text-lg mb-12 text-center mx-auto">
+            {REVIEWS.text}
+          </p>
+          <div className="flex flex-wrap justify-center">
+            {REVIEWS.reviews.map((review, index) => (
+              <div key={index} className="mt-10 flex flex-col items-center justify-center 
+              rounded-2xl border border-neutral-300 p-10 mx-2 max-w-xs">
+                <p className="mb-4">{review.review}</p>
+              </div>
+            ))}
+          </div>
         </div>
     </section>
   )
